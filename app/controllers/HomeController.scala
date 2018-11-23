f24d7432297fc6196fa7ff113aa8a6dbe3f8b149
Vincent Restrepo <vdrestrepo@geosat.com.co>
2018-11-23 18:54:17 -0500

    https://github.com/playframework/play-slick

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,7 +2,11 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.mvc._
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -25,4 +29,20 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def suma(num1: Int, num2: Int) = Action { implicit request =>
     Ok(s"${num1+num2*3}")
   }
+
+}
+
+class Application @Inject() (
+
+                              protected val dbConfigProvider: DatabaseConfigProvider,
+                              cc: ControllerComponents
+                            )(implicit ec: ExecutionContext)
+  extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
+  import dbConfig.profile.api._
+
+  def index(name: String) = Action.async { implicit request =>
+    val resultingUsers: Future[Seq[User]] = db.run(Users.filter(_.name === name).result)
+    resultingUsers.map(users => Ok(views.html.index(users)))
+  }
+
 }
--- build.sbt
+++ build.sbt
@@ -9,6 +9,10 @@ scalaVersion := "2.12.6"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
+libraryDependencies += "com.typesafe.play" %% "play-slick" % "3.0.0"
+
+// https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
+libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.25.2"
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "com.vince.controllers._"
